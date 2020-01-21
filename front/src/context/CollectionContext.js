b72cf5237c7eabdf4d10722b4f28cbb05c1552cb
reshreshus <reshreshus@gmail.com>
2020-01-21 23:39:33 +0300

    add / delete card entries

--- front/src/context/CollectionContext.js
+++ front/src/context/CollectionContext.js
@@ -1,40 +1,83 @@
 import React from 'react'
 
 // import card from '../desc';
-const card = {
-    id: "database generated",
-    deck_id: "from db",
-    template_id: "from db",
-    deck_title: "Enlish",
-    template_title: "Basic",
-    entries: [
-        {
-            entry_id: "1",
-            content: "probably some editorJs stuff or html",
-            isQuestion: true,
-        },
-        {
-            entry_id: "2",
-            content: "probably some editorJs stuff or html",
-            isQuestion: false
-        }
-    ]
+
+
+const updateCardEntries = (cardId, changes) => {
+    console.log("card is updating (supposedly)", changes)
+    console.log("card", card);
 }
 
-const updateCard = (changes) => {
-    console.log("card is updating (supposedly)", changes)
-}
+
 
 
 const Collection = React.createContext();
 
 class CollectionProvider extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+             card: {
+                id: "database generated",
+                deck_id: "from db",
+                template_id: "from db",
+                deck_title: "Enlish",
+                template_title: "Basic",
+                entries: [
+                    {
+                        entry_id: 0,
+                        content: "probably some editorJs stuff or html",
+                        isQuestion: true,
+                    },
+                    {
+                        entry_id: 1,
+                        content: "probably some editorJs stuff or html",
+                        isQuestion: false
+                    },
+                    
+                ]
+            }
+        };
+    }
+
+    addNewEntryContext = (cardId) => {
+        console.log("addNewEntryContext")
+        let newCard = this.state.card;
+        newCard.entries = [...this.state.card.entries, {
+            entry_id: this.state.card.entries.length,
+            content: "probably some editorJs stuff or html",
+            isQuestion: false
+        }]
+        this.setState({
+            card: newCard
+        }, () => {
+            console.log("state", this.state);
+        })
+    }
+
+    deleteEntryContext = (cardId, entryId) => {
+        let newCard = this.state.card;
+        newCard.entries = this.state.card.entries;
+        newCard.entries.splice(entryId, 1)
+        newCard.entries.map((e) => {
+            if (e.entry_id > entryId) {
+                e.entry_id--;
+            }
+        })
+        this.setState({
+            card: newCard
+        }, () => {
+            console.log("state", this.state);
+        })
+    }
+
     render () {
-        console.log("desc card", card);
         return (
             <Collection.Provider value={{
-                    card,
-                    updateCard
+                    card: this.state.card,
+                    updateCardEntries,
+                    addNewEntryContext: this.addNewEntryContext,
+                    deleteEntryContext: this.deleteEntryContext
             }}>
                 {this.props.children}
             </Collection.Provider>
--- front/src/pages/Editor.js
+++ front/src/pages/Editor.js
@@ -5,73 +5,78 @@ import EditorJs from 'react-editor-js';
 import { EDITOR_JS_TOOLS } from './editorJsTools'  
 
 export default function Editor() {
-    
    
 return (<CollectionConsumer >
-    { (value) => {
-        const {deck_title, template_title, entries} = value.card;
-        const entries_editors = new Array(entries.length)
-        const handleSave = async () => {
-            // TODO: You can utilize smart way of changing stuff like github
-            const newCardEntries = []
-            entries_editors.map(async (editor) => {
-                const savedData = await editor.save();
-                newCardEntries.push([...savedData.blocks])
-            })
-            // console.log("newCardEntries", newCardEntries)
-            // what if update template / deck ?
-            const changes = {
-                "newCardEntries": newCardEntries
-            }
-            value.updateCard(changes);
-        }    
-        return (
-            <div className="editor">
-                <div className="editor__header">
-                    <div className="editor__header-left">
-                        <div className="editor__subtitle text-blue-bright"> Deck </div>
-                        <div className="editor__title text-dark">
-                            {deck_title}
-                        </div>
-                    </div>
-                    <div className="editor__header-right">
-                        <div className="editor__subtitle text-blue-bright"> Template </div>
-                        <div className="editor__title text-dark">
-                            {template_title}
-                        </div>
-                    </div>
-                </div>
+    { ({card, updateCardEntries, addNewEntryContext, deleteEntryContext}) => {
+    const {deck_title, template_title, entries} = card;
+    const entries_editors = new Array(entries.length)
+    
+    const saveCardEntries = async () => { // TODO: You can utilize smart way of changing stuff like github
+        const newCardEntries = []
+        entries_editors.map(async (editor) => {
+            const savedData = await editor.save();
+            newCardEntries.push([...savedData.blocks])
+        })
+        const changes = {
+            "newCardEntries": newCardEntries
+        }
+        updateCardEntries(card.id, changes);
+    }
 
-                <div className="editor__entries">
-                    {   entries ?
-                        entries.map((e, i) => (
-                            <div className="card-entry" key={i}>
-                                <div className="card-entry__qa btn btn-circ">
-                                {
-                                    e.isQuestion ? 
-                                    "Q" : "A"
-                                }
-                                </div>
-                                <div className="" className="text-field" 
-                                    id={"editor-js-" + e.entry_id}>
-                                    <EditorJs 
-                                        instanceRef={instance => entries_editors[i] = instance}
-                                        tools={EDITOR_JS_TOOLS}
-                                        holder={"editor-js-" + e.entry_id}
-                                    />
-                                </div>
-                                <div className="btn btn-circ btn-plus-minus">-</div>
-                            </div>
-                        )) : "Hmm, a card is empty. Strange..."
-                    }
+    const addNewEntry = () => {
+        console.log("adding new entry")
+        addNewEntryContext(card.id)
+    }
+
+    const deleteEntry = (entryId) => {
+        deleteEntryContext(card.id, entryId)
+    }
+    return (
+        <div className="editor">
+            <div className="editor__header">
+                <div className="editor__header-left">
+                    <div className="editor__subtitle text-blue-bright"> Deck </div>
+                    <div className="editor__title text-dark">
+                        {deck_title}
+                    </div>
                 </div>
-                <div className="editor__actions">
-                    <div className="btn btn-circ btn-plus-minus">+</div>
-                    <div onClick={() => handleSave()} className="btn btn-primary">Save</div>
+                <div className="editor__header-right">
+                    <div className="editor__subtitle text-blue-bright"> Template </div>
+                    <div className="editor__title text-dark">
+                        {template_title}
+                    </div>
                 </div>
             </div>
-        )
-        }
+            <div className="editor__entries">
+                {   entries ?
+                    entries.map((e, i) => (
+                        <div className="card-entry" key={i}>
+                            <div className="card-entry__qa btn btn-circ">
+                            {
+                                e.isQuestion ? 
+                                "Q" : "A"
+                            }
+                            </div>
+                            <div className="" className="text-field" 
+                                id={"editor-js-" + e.entry_id}>
+                                <EditorJs 
+                                    instanceRef={instance => entries_editors[i] = instance}
+                                    tools={EDITOR_JS_TOOLS}
+                                    holder={"editor-js-" + e.entry_id}
+                                />
+                            </div>
+                            <div onClick={() => deleteEntry(e.entry_id)} className="btn btn-circ btn-plus-minus">-</div>
+                        </div>
+                    )) : "Hmm, a card is empty. Strange..."
+                }
+            </div>
+            <div className="editor__actions">
+                <div onClick={() => addNewEntry()}className="btn btn-circ btn-plus-minus">+</div>
+                <div onClick={() => saveCardEntries()} className="btn btn-primary">Save</div>
+            </div>
+        </div>
+    )
+    }
     }
 </CollectionConsumer>)
 }
