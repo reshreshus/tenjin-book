40017ed30b43b4ad9e2c606706f9d5061b09a70c
reshreshus <reshreshus@gmail.com>
2020-01-25 03:43:55 +0300

    add header emoji

--- front/src/components/Sidebar/Header.js
+++ front/src/components/Sidebar/Header.js
@@ -10,7 +10,8 @@ export default function Header() {
                 return (
                     <div className="header">
                         <Link to="/" className="link" onClick={() => updateSelectedBlockId('')}>
-                            <h1>Gora Gora</h1>
+                            <h1>Gora Gora <br/>⟅(☉ ⃢ ☉)⟆
+                            </h1>
                         </Link>
                     </div>
                 )
