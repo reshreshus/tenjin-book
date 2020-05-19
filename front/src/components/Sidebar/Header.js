d0ed8a1cf41319402aa5789054868ade5e1fc772
reshreshus <reshreshus@gmail.com>
2020-05-19 22:49:23 +0300

    use a weird braille icon instead of ellipsis

--- front/src/components/Sidebar/Header.js
+++ front/src/components/Sidebar/Header.js
@@ -3,6 +3,9 @@ import {Link} from 'react-router-dom'
 import { CollectionConsumer } from '../../context/CollectionContext';
 import dots from '../../assets/svg/dots-three-vertical.svg'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBraille } from '@fortawesome/free-solid-svg-icons';
+
 export default function Header() {
   return (
     <CollectionConsumer>
@@ -12,10 +15,16 @@ export default function Header() {
           <div className="header">
             <Link to="/" className="header__link link" onClick={() => updateContextTreeItem(null)}>
               <img className="header__img" src="tenjinbook.png" alt="header"/>
-              <h1 className="header__title spade-border">Tenjin Book </h1>
+              <h1 className="header__title">Tenjin Book </h1>
             </Link>
-            <img className="header__options" src={dots} alt="dots" height="28px"
-              onClick={(e) => openAppContextMenu(e)}/>
+            <FontAwesomeIcon
+                className="header__options"
+                icon={faBraille}
+                size="lg"
+                onClick={(e) => openAppContextMenu(e)}
+                />
+            {/* <img className="header__options" src={dots} alt="dots" height="28px"
+              onClick={(e) => openAppContextMenu(e)}/> */}
           </div>
         )
       }
