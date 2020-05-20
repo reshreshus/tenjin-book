7f65345984e68f4f6742cfb1d55df2f4f3712d7b
reshreshus <reshreshus@gmail.com>
2020-05-21 00:03:14 +0300

    get entry bg back

--- front/src/components/Editor/EditorActions.js
+++ front/src/components/Editor/EditorActions.js
@@ -4,7 +4,7 @@ const EditorActions = ({saveCard, addNewEntry, editorChanged, card}) => {
   return (
     <div className="editor__actions">
       <div onClick={() => addNewEntry(card.id)}className="btn btn-circ btn-plus-minus">+</div>
-      <div onClick={() => saveCard()} className="btn btn-text">Save{editorChanged ? "*": ""}</div>
+      <div onClick={() => saveCard()} className="btn btn-ed">Save{editorChanged ? "*": ""}</div>
     </div>
   );
 }
