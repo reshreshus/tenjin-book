ea24f0de0c9a77123c8e34b3e18f516485ea5ca4
reshreshus <reshreshus@gmail.com>
2020-05-25 17:59:49 +0300

    :lipstick: button

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -4,9 +4,9 @@ import { ReactComponent as TenjinIcon } from '../assets/svg/tenjin.svg';
 export default function Login() {
   return (
     <div className="login">
-      <div className="login-form">
-        <TenjinIcon className="login-form__img"/>
-        <form>
+      <div >
+        <form className="login-form">
+          <TenjinIcon className="login-form__img"/>
           <div className="field">
             <input type="email" name="email" className="field__input" placeholder=" "/>
             <label for="email" className="field__label"> Email </label>
@@ -16,7 +16,7 @@ export default function Login() {
             <label for="password" className="field__label"> Password </label>
           </div>
 
-          <button>Sign Up</button>
+          <div className="btn login-form__btn">Sign Up</div>
         </form>
       </div>
     </div>
