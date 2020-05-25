261b8ab22e96c91e6fee2a4527fc72bfe1fb1ae9
reshreshus <reshreshus@gmail.com>
2020-05-25 18:23:26 +0300

    :lipstick: + register page

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ReactComponent as TenjinIcon } from '../assets/svg/tenjin.svg';
 
 export default function Login() {
+  const [isLogin, updateIsLogin] = useState(true)
   return (
     <div className="login">
-      <div >
-        <form className="login-form">
-          <TenjinIcon className="login-form__img"/>
-          <div className="field">
-            <input type="email" name="email" className="field__input" placeholder=" "/>
-            <label for="email" className="field__label"> Email </label>
-          </div>
-          <div className="field">
-            <input type="password" name="password" className="field__input" placeholder=" "/>
-            <label for="password" className="field__label"> Password </label>
-          </div>
+      {
+        isLogin && <form className="login-form">
+        <TenjinIcon className="login-form__img"/>
+        <div className="field">
+          <input type="email" name="email" className="field__input" placeholder=" "/>
+          <label for="email" className="field__label"> Email </label>
+        </div>
+        <div className="field">
+          <input type="password" name="password" className="field__input" placeholder=" "/>
+          <label for="password" className="field__label"> Password </label>
+        </div>
 
-          <div className="btn login-form__btn">Sign Up</div>
-        </form>
-      </div>
+        <div className="btn login-form__btn">Sign In</div>
+        <div className="btn login-form__option" onClick={() => updateIsLogin(false)}> or <span>Register</span></div>
+      </form>
+      }
+      {
+        !isLogin && <form className="login-form">
+        <TenjinIcon className="login-form__img"/>
+        <div className="field">
+          <input type="username" name="username" className="field__input" placeholder=" "/>
+          <label for="username" className="field__label"> Email </label>
+        </div>
+        <div className="field">
+          <input type="email" name="email" className="field__input" placeholder=" "/>
+          <label for="email" className="field__label"> Email </label>
+        </div>
+        <div className="field">
+          <input type="password" name="password" className="field__input" placeholder=" "/>
+          <label for="password" className="field__label"> Password </label>
+        </div>
+
+        <div className="btn login-form__btn">Register</div>
+        <div className="btn login-form__option" onClick={() => updateIsLogin(true)}> or <span>Sign In</span></div>
+      </form>
+      }
     </div>
   )
 }
