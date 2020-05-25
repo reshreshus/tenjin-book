f23fab6f3f1a0289ba118146617016be05f79d97
reshreshus <reshreshus@gmail.com>
2020-05-25 17:07:53 +0300

    add /login page

--- front/src/App.js
+++ front/src/App.js
@@ -1,33 +1,16 @@
 import React, {useEffect} from 'react'
 import './styles/main.sass'
 import { Switch, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar/Sidebar';
-import AppStats from './components/AppStats';
-
-import Home from './pages/Home';
-import Edit from './pages/Edit';
-import ShowDeck from './pages/ShowDeck';
-
+import Game from './Game';
+import Login from './pages/Login';
 
 export default function App() {
   return (
     <div className="app">
-      <Sidebar />
-      <div className="resizer"/>
-      <div className="right-sidebar">
-        <div className="right-sidebar__header">
-          <AppStats />
-        </div>
-        <div className="right-sidebar__content">
-          <Switch>
-            <Route exact path="/edit/" component={() =>
-              (<Edit card={{'deckTitle':'English', 'templateTitle': 'Basic'}}/>)}
-              />
-            <Route exact path="/show-deck/" component={ShowDeck} />
-            <Route exact path="/" component={Home} />
-          </Switch>
-        </div>
-      </div>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route path="/*" component={Game} />
+      </Switch>
     </div>
   )
 }
