e6290120e28d8a37174d7fa3105b6c86b1cdd540
reshreshus <reshreshus@gmail.com>
2020-05-25 17:19:21 +0300

    :recylce: sass

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -3,16 +3,20 @@ import React from 'react'
 export default function Login() {
   return (
     <div className="login">
-      <form>
-        <div className="field">
-          <input type="email" name="email" className="field__input" placeholder=" "/>
-          <label for="email" className="field__label"> Email </label>
-        </div>
-        <div className="field">
-          <input type="password" name="password" className="field__input" placeholder=" "/>
-          <label for="password" className="field__label"> Password </label>
-        </div>
-      </form>
+      <div className="form">
+        <form>
+          <div className="field">
+            <input type="email" name="email" className="field__input" placeholder=" "/>
+            <label for="email" className="field__label"> Email </label>
+          </div>
+          <div className="field">
+            <input type="password" name="password" className="field__input" placeholder=" "/>
+            <label for="password" className="field__label"> Password </label>
+          </div>
+
+          <button>Sign Up</button>
+        </form>
+      </div>
     </div>
   )
 }
