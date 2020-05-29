7ad551f5f1534bb52a29d0c4807e3176a3d1c430
reshreshus <reshreshus@gmail.com>
2020-05-29 15:46:23 +0300

    + authen. queries, code 4 spaces to 2 in  api code

--- front/src/api/queries.js
+++ front/src/api/queries.js
@@ -1,101 +1,117 @@
 import { gql } from 'apollo-boost';
 
-export const ADVANCE_CARD = gql`
-    mutation advanceCard($id: String!, $quality: Int!) {
-        advanceCard(id: $id, quality: $quality)
+export const REGISTER = gql`
+  mutation register($email: String!, $username: String!, $password: String!) {
+    register(email: $email, username: $username, password: $password) {
+      username
     }
+  }
+`
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`
+
+export const ADVANCE_CARD = gql`
+  mutation advanceCard($id: String!, $quality: Int!) {
+    advanceCard(id: $id, quality: $quality)
+  }
 `
 
 export const ADD_DECK = gql`
-    mutation addDeck($parentId: String!) {
-        addDeck(parentId: $parentId)
-    }
+  mutation addDeck($parentId: String!) {
+    addDeck(parentId: $parentId)
+  }
 `
 
 export const DUPLICATE_TREE_ITEM = gql`
-    mutation duplicateTreeItem($id: String!) {
-        duplicateTreeItem(id: $id)
-    }
+  mutation duplicateTreeItem($id: String!) {
+    duplicateTreeItem(id: $id)
+  }
 `
 
 export const DELETE_TREE_ITEM = gql`
-    mutation deleteTreeItem($id: String!) {
-        deleteTreeItem(id: $id)
-    }
+  mutation deleteTreeItem($id: String!) {
+    deleteTreeItem(id: $id)
+  }
 `
 
 export const RENAME_TREE_ITEM = gql`
-    mutation renameTreeItem($id: String!, $newName: String!) {
-        renameTreeItem(id: $id, newName: $newName) {
-            data {
-                name
-            }
-        }
+  mutation renameTreeItem($id: String!, $newName: String!) {
+    renameTreeItem(id: $id, newName: $newName) {
+      data {
+        name
+      }
     }
+  }
 `
 
 export const ADD_ITEM = gql`
-    mutation addItem($type: String!, $parentId: String!){
-        addItem(type: $type, parentId: $parentId)
-    }
+  mutation addItem($type: String!, $parentId: String!){
+    addItem(type: $type, parentId: $parentId)
+  }
 `
 export const SAVE_TREE = gql`
-    mutation Tree($newTree: JSON) {
-        saveTree (newTree: $newTree)
-    }
+  mutation Tree($newTree: JSON) {
+    saveTree (newTree: $newTree)
+  }
 `;
 
 export const GET_ITEMS = gql`
-    mutation {
-        items
-    }
+  mutation {
+    items
+  }
 `;
 
 export const GET_TREE = gql`
-    {
-        tree
-    }
+  {
+    tree
+  }
 `;
 
 export const GET_CARD = gql`
-    mutation Card($id: ID!) {
-        card (id: $id) {
-            id
-            templateTitle
-            entries {
-                id
-                name
-                content
-                type
-                format
-            }
-        }
+  mutation Card($id: ID!) {
+    card (id: $id) {
+      id
+      templateTitle
+      entries {
+        id
+        name
+        content
+        type
+        format
+      }
     }
+  }
 `;
 
 export const SAVE_CARD = gql`
-    mutation SaveCard($id: ID!, $templateTitle:String!, $entries: [JSON]) {
-            saveCard(id: $id, templateTitle: $templateTitle, entries: $entries) {
-            id
-            entries {
-                content
-                format
-            }
-        }
+  mutation SaveCard($id: ID!, $templateTitle:String!, $entries: [JSON]) {
+      saveCard(id: $id, templateTitle: $templateTitle, entries: $entries) {
+      id
+      entries {
+        content
+        format
+      }
     }
+  }
 `;
 
 export const ADD_CARD_ENTRY = gql`
-    mutation AddCardEntry($id: Int!, $name: String!, $content: JSON!, $type: String!, $cardId: ID!) {
-        addCardEntry(id: $id, name: $name, content: $content, type: $type, cardId: $cardId) {
-            id
-            content
-        }
+  mutation AddCardEntry($id: Int!, $name: String!, $content: JSON!, $type: String!, $cardId: ID!) {
+    addCardEntry(id: $id, name: $name, content: $content, type: $type, cardId: $cardId) {
+      id
+      content
     }
+  }
 `;
 
 export const BACKUP = gql`
-    mutation Backup {
-        backup
-    }
+  mutation Backup {
+    backup
+  }
 `
--- front/src/api/rest.js
+++ front/src/api/rest.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
-const serverUrl = 'http://localhost:5000'
+const serverUrl = 'http://localhost:4000'
 
 export const uploadDeckImage = async (deckId, imageFile) => {
-    const formData = new FormData();
-    formData.append("image", imageFile);
-    formData.append("id", deckId);
+  const formData = new FormData();
+  formData.append("image", imageFile);
+  formData.append("id", deckId);
 
-    const { data } = await axios.post(`${serverUrl}/uploadByFile`, formData, {
-        headers: {
-        'Content-Type': 'multipart/form-data'
-        }
-    });
-    return data.file.url;
+  const { data } = await axios.post(`${serverUrl}/uploadByFile`, formData, {
+    headers: {
+    'Content-Type': 'multipart/form-data'
+    }
+  });
+  return data.file.url;
 }
 
 export const deleteImage = async (url) => {
-    const { data } = await axios.delete(url);
-    return data.status;
+  const { data } = await axios.delete(url);
+  return data.status;
 }
