87f88f53fb24c7f174383865ba64b51314eb8153
reshreshus <reshreshus@gmail.com>
2020-06-03 18:39:11 +0300

    :bug: wrong ApolloClient used

--- front/src/index.js
+++ front/src/index.js
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router} from 'react-router-dom';
 import ApiContext from './context/ApiContext'
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client'
 import { ApolloProvider } from '@apollo/react-hooks';
 
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const link = new HttpLink({
+  uri: "http://localhost:4000/graphql"
+});
+
+
+
 const client = new ApolloClient({
-    uri:'http://localhost:4000/graphql'
-  })
+  link,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
     <ApolloProvider client={client}>
