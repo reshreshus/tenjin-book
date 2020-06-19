e78cb6eeda46098f2843130c69a2e67ce4fe691c
reshreshus <reshreshus@gmail.com>
2020-06-19 18:15:19 +0300

    - fontawesome imports

--- front/src/components/Sidebar/Items.js
+++ front/src/components/Sidebar/Items.js
@@ -11,9 +11,6 @@ import tType from '../../assets/svg/t-type.svg';
 import tTypeAdd from '../../assets/svg/t-type-add.svg';
 import addFolder from '../../assets/svg/add-folder.svg';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFolderPlus } from '@fortawesome/free-solid-svg-icons';
-
 const renderItem = ({
   item: treeItem,
   onExpand,
